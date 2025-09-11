5656dc6678230029d96a4e206c2e5e3bfaf471aa
Antonio Gonzalez <antgonza@gmail.com>
2025-09-11 13:48:42 -0600

    adding real test

--- qp_pacbio/qp_pacbio.py
+++ qp_pacbio/qp_pacbio.py
@@ -6,10 +6,14 @@
 # The full license is in the file LICENSE, distributed with this software.
 # -----------------------------------------------------------------------------
 from qiita_client import ArtifactInfo
+from jinja2 import Environment
+from jinja2 import BaseLoader, TemplateNotFound
+from os.path import basename, join, exists, getmtime
 from os import makedirs
-from os.path import basename
+import pathlib
 
 
+# taken from qp-woltka
 def search_by_filename(fname, lookup):
     if fname in lookup:
         return lookup[fname]
@@ -31,6 +35,87 @@ def search_by_filename(fname, lookup):
             return lookup[rp]
 
     raise KeyError("Cannot determine run_prefix for %s" % original)
+
+
+# taken from https://jinja.palletsprojects.com/en/3.0.x/api/#jinja2.BaseLoader
+class KISSLoader(BaseLoader):
+    def __init__(self, path):
+        # pin the path for loader to the location sequence_processing_pipeline
+        # (the location of this file), along w/the relative path to the
+        # templates directory.
+        self.path = join(pathlib.Path(__file__).parent.resolve(), path)
+
+    def get_source(self, environment, template):
+        path = join(self.path, template)
+        if not exists(path):
+            raise TemplateNotFound(template)
+        mtime = getmtime(path)
+        with open(path) as f:
+            source = f.read()
+        return source, path, lambda: mtime == getmtime(path)
+
+
+def generate_templates(out_dir, job_id, njobs):
+    """Generates the templates
+
+    Parameters
+    ----------
+    out_dir : str
+        The path to the job's output directory
+    job_id : str
+        The job id
+    njobs : int
+        The number of total jobs
+    """
+
+    jinja_env = Environment(loader=KISSLoader('../data/templates'))
+    template = jinja_env.get_template("1.hifiasm-meta_new.sbatch")
+    cdir = f'{out_dir}/step-1'
+    makedirs(cdir)
+    with open(f'{cdir}/step-1.slurm', mode="w", encoding="utf-8") as f:
+        f.write(template.render(
+            conda_environment='qp_pacbio_2025.9',
+            output=f'{out_dir}',
+            job_name=f's1-{job_id}',
+            node_count=1,
+            nprocs=16,
+            wall_time_limit=1000,
+            mem_in_gb=16,
+            array_params=f'1:{njobs}%16'
+        ))
+
+
+def generate_sample_list(qclient, artifact_id, out_dir):
+    """Generates the sample_list.txt file
+
+    Parameters
+    ----------
+    qclient : tgp.qiita_client.QiitaClient
+        The Qiita server client
+    artifact_id : int
+        The artifact id
+    out_dir : str
+        The path to the job's output directory
+
+    Returns
+    -------
+    int
+        The number of jobs/lines without header in sample_list.txt
+    """
+    files, prep = qclient.artifact_and_preparation_files(artifact_id)
+
+    lookup = prep.set_index('run_prefix')['sample_name'].to_dict()
+    lines = ['sample_name\tfilename']
+    for _, (fwd, _) in files.items():
+        fwd = fwd['filepath']
+        sname = search_by_filename(basename(fwd), lookup)
+        lines.append(f'{sname}\t{fwd}')
+
+    with open(f'{out_dir}/sample_list.txt', 'w') as f:
+        f.write('\n'.join(lines))
+    njobs = len(lines) - 1
+
+    return njobs
 
 
 def pacbio_processing(qclient, job_id, parameters, out_dir):
@@ -58,23 +143,18 @@ def pacbio_processing(qclient, job_id, parameters, out_dir):
     qclient.update_job_step(
             job_id, "Step 1 of 3: Collecting info and generating submission")
     artifact_id = parameters['artifact_id']
-    files, prep = qclient.artifact_and_preparation_files(artifact_id)
-    lookup = prep.set_index('run_prefix')['sample_name'].to_dict()
-    lines = ['sample_name\tfilename']
-    for _, (fwd, _) in files.items():
-        sname = search_by_filename(basename(fwd['filepath']), lookup)
-        lines.append(f'{sname}\t{fwd}')
 
-    with open(f'{out_dir}/sample_list.txt') as f:
-        f.write('\n'.append(lines))
+    njobs = generate_sample_list(qclient, artifact_id, out_dir)
 
     qclient.update_job_step(
             job_id, "Step 2 of 3: Creating submission templates")
-    # TODO 1. add code to create templates
-    # TODO 2. submit templates
+
+    njobs = generate_sample_list(qclient, artifact_id, out_dir)
+    generate_templates(out_dir, job_id, njobs)
 
     qclient.update_job_step(
             job_id, "Step 3 of 3: Running commands")
+
     # TODO 3. wait for all jobs to finish
 
     qclient.update_job_step(
