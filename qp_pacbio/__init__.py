03e3d94be68eb58aa8f4a9ebec2ebe4ff02b6cab
jianshu93 <jansuechao@gmail.com>
2025-10-04 16:48:12 -0700

    format

--- qp_pacbio/__init__.py
+++ qp_pacbio/__init__.py
@@ -8,20 +8,25 @@
 from qiita_client import QiitaPlugin, QiitaCommand
 from .qp_pacbio import pacbio_processing
 
-__version__ = '2025.9'
+__version__ = "2025.9"
 
-plugin = QiitaPlugin('qp-pacbio', __version__, 'PacBio Processing')
+plugin = QiitaPlugin("qp-pacbio", __version__, "PacBio Processing")
 
 req_params = {
-    'artifact_id': ('integer', [None]),
+    "artifact_id": ("integer", [None]),
 }
 opt_params = dict()
-outputs = {'output': 'job-output-folder'}
+outputs = {"output": "job-output-folder"}
 dflt_param_set = dict()
 
 pacbio_processing_cmd = QiitaCommand(
-    'PacBio processing', 'Default PacBio processing for Metagenomic Data',
-    pacbio_processing, req_params, opt_params, outputs,
-    dflt_param_set)
+    "PacBio processing",
+    "Default PacBio processing for Metagenomic Data",
+    pacbio_processing,
+    req_params,
+    opt_params,
+    outputs,
+    dflt_param_set,
+)
 
 plugin.register_command(pacbio_processing_cmd)
--- qp_pacbio/qp_pacbio.py
+++ qp_pacbio/qp_pacbio.py
@@ -18,14 +18,14 @@ def search_by_filename(fname, lookup):
         return lookup[fname]
 
     original = fname
-    while '_' in fname:
-        fname = fname.rsplit('_', 1)[0]
+    while "_" in fname:
+        fname = fname.rsplit("_", 1)[0]
         if fname in lookup:
             return lookup[fname]
 
     fname = original
-    while '.' in fname:
-        fname = fname.rsplit('.', 1)[0]
+    while "." in fname:
+        fname = fname.rsplit(".", 1)[0]
         if fname in lookup:
             return lookup[fname]
 
@@ -65,137 +65,151 @@ def generate_templates(out_dir, job_id, njobs):
         The number of total jobs
     """
 
-    jinja_env = Environment(loader=KISSLoader('../data/templates'))
+    jinja_env = Environment(loader=KISSLoader("../data/templates"))
 
     template0 = jinja_env.get_template("0.mapping_minimap2_db.sbatch")
-    cdir0 = f'{out_dir}/step-0'
+    cdir0 = f"{out_dir}/step-0"
     makedirs(cdir0)
-    makedirs(f'{cdir0}/logs', exist_ok=True)
-    with open(f'{cdir0}/step-0.slurm', mode="w", encoding="utf-8") as f:
-        f.write(template0.render(
-            conda_environment='qp_pacbio_2025.9',
-            output=f'{out_dir}',
-            job_name=f's0-{job_id}',
-            node_count=1,
-            nprocs=16,
-            wall_time_limit=1000,
-            mem_in_gb=300,
-            array_params=f'1-{njobs}%16'
-        ))
-
+    makedirs(f"{cdir0}/logs", exist_ok=True)
+    with open(f"{cdir0}/step-0.slurm", mode="w", encoding="utf-8") as f:
+        f.write(
+            template0.render(
+                conda_environment="qp_pacbio_2025.9",
+                output=f"{out_dir}",
+                job_name=f"s0-{job_id}",
+                node_count=1,
+                nprocs=16,
+                wall_time_limit=1000,
+                mem_in_gb=300,
+                array_params=f"1-{njobs}%16",
+            )
+        )
 
     template = jinja_env.get_template("1.hifiasm-meta_new.sbatch")
-    cdir = f'{out_dir}/step-1'
+    cdir = f"{out_dir}/step-1"
     makedirs(cdir)
-    makedirs(f'{cdir}/logs', exist_ok=True)
-    with open(f'{cdir}/step-1.slurm', mode="w", encoding="utf-8") as f:
-        f.write(template.render(
-            conda_environment='qp_pacbio_2025.9',
-            output=f'{out_dir}',
-            job_name=f's1-{job_id}',
-            node_count=1,
-            nprocs=16,
-            wall_time_limit=1000,
-            mem_in_gb=300,
-            array_params=f'1-{njobs}%16'
-        ))
+    makedirs(f"{cdir}/logs", exist_ok=True)
+    with open(f"{cdir}/step-1.slurm", mode="w", encoding="utf-8") as f:
+        f.write(
+            template.render(
+                conda_environment="qp_pacbio_2025.9",
+                output=f"{out_dir}",
+                job_name=f"s1-{job_id}",
+                node_count=1,
+                nprocs=16,
+                wall_time_limit=1000,
+                mem_in_gb=300,
+                array_params=f"1-{njobs}%16",
+            )
+        )
 
     template2 = jinja_env.get_template("2.get-circular-genomes.sbatch")
-    cdir2 = f'{out_dir}/step-2'
+    cdir2 = f"{out_dir}/step-2"
     makedirs(cdir2)
-    makedirs(f'{cdir2}/logs', exist_ok=True)
-    with open(f'{cdir2}/step-2.slurm', mode="w", encoding="utf-8") as f:
-        f.write(template2.render(
-            conda_environment='qp_pacbio_2025.9',
-            output=f'{out_dir}',
-            job_name=f's2-{job_id}',
-            node_count=1,
-            nprocs=1,
-            wall_time_limit=500,
-            mem_in_gb=16,
-            array_params=f'1-{njobs}%16'
-        ))
+    makedirs(f"{cdir2}/logs", exist_ok=True)
+    with open(f"{cdir2}/step-2.slurm", mode="w", encoding="utf-8") as f:
+        f.write(
+            template2.render(
+                conda_environment="qp_pacbio_2025.9",
+                output=f"{out_dir}",
+                job_name=f"s2-{job_id}",
+                node_count=1,
+                nprocs=1,
+                wall_time_limit=500,
+                mem_in_gb=16,
+                array_params=f"1-{njobs}%16",
+            )
+        )
 
     template3 = jinja_env.get_template("3.minimap2_assembly.sbatch")
-    cdir3 = f'{out_dir}/step-3'
+    cdir3 = f"{out_dir}/step-3"
     makedirs(cdir3)
-    makedirs(f'{cdir3}/logs', exist_ok=True)
-    with open(f'{cdir3}/step-3.slurm', mode="w", encoding="utf-8") as f:
-        f.write(template3.render(
-            conda_environment='qp_pacbio_2025.9',
-            output=f'{out_dir}',
-            job_name=f's3-{job_id}',
-            node_count=1,
-            nprocs=8,
-            wall_time_limit=500,
-            mem_in_gb=50,
-            array_params=f'1-{njobs}%16'
-        ))
+    makedirs(f"{cdir3}/logs", exist_ok=True)
+    with open(f"{cdir3}/step-3.slurm", mode="w", encoding="utf-8") as f:
+        f.write(
+            template3.render(
+                conda_environment="qp_pacbio_2025.9",
+                output=f"{out_dir}",
+                job_name=f"s3-{job_id}",
+                node_count=1,
+                nprocs=8,
+                wall_time_limit=500,
+                mem_in_gb=50,
+                array_params=f"1-{njobs}%16",
+            )
+        )
 
     template4 = jinja_env.get_template("4.metawrap_binning_new.sbatch")
-    cdir4 = f'{out_dir}/step-4'
+    cdir4 = f"{out_dir}/step-4"
     makedirs(cdir4)
-    makedirs(f'{cdir4}/logs', exist_ok=True)
-    with open(f'{cdir4}/step-4.slurm', mode="w", encoding="utf-8") as f:
-        f.write(template4.render(
-            conda_environment='qp_pacbio_2025.9',
-            output=f'{out_dir}',
-            job_name=f's4-{job_id}',
-            node_count=1,
-            nprocs=8,
-            wall_time_limit=500,
-            mem_in_gb=50,
-            array_params=f'1-{njobs}%16'
-        ))
-    
+    makedirs(f"{cdir4}/logs", exist_ok=True)
+    with open(f"{cdir4}/step-4.slurm", mode="w", encoding="utf-8") as f:
+        f.write(
+            template4.render(
+                conda_environment="qp_pacbio_2025.9",
+                output=f"{out_dir}",
+                job_name=f"s4-{job_id}",
+                node_count=1,
+                nprocs=8,
+                wall_time_limit=500,
+                mem_in_gb=50,
+                array_params=f"1-{njobs}%16",
+            )
+        )
+
     template5 = jinja_env.get_template("5.DAS_Tools_prepare_batch3_test.sbatch")
-    cdir5 = f'{out_dir}/step-5'
+    cdir5 = f"{out_dir}/step-5"
     makedirs(cdir5)
-    makedirs(f'{cdir5}/logs', exist_ok=True)
-    with open(f'{cdir5}/step-5.slurm', mode="w", encoding="utf-8") as f:
-        f.write(template5.render(
-            conda_environment='qp_pacbio_2025.9',
-            output=f'{out_dir}',
-            job_name=f's5-{job_id}',
-            node_count=1,
-            nprocs=8,
-            wall_time_limit=500,
-            mem_in_gb=50,
-            array_params=f'1-{njobs}%16'
-        ))
+    makedirs(f"{cdir5}/logs", exist_ok=True)
+    with open(f"{cdir5}/step-5.slurm", mode="w", encoding="utf-8") as f:
+        f.write(
+            template5.render(
+                conda_environment="qp_pacbio_2025.9",
+                output=f"{out_dir}",
+                job_name=f"s5-{job_id}",
+                node_count=1,
+                nprocs=8,
+                wall_time_limit=500,
+                mem_in_gb=50,
+                array_params=f"1-{njobs}%16",
+            )
+        )
 
     template6 = jinja_env.get_template("6.MAG_rename.sbatch")
-    cdir6 = f'{out_dir}/step-6'
+    cdir6 = f"{out_dir}/step-6"
     makedirs(cdir6)
-    makedirs(f'{cdir6}/logs', exist_ok=True)
-    with open(f'{cdir6}/step-6.slurm', mode="w", encoding="utf-8") as f:
-        f.write(template6.render(
-            conda_environment='qp_pacbio_2025.9',
-            output=f'{out_dir}',
-            job_name=f's6-{job_id}',
-            node_count=1,
-            nprocs=8,
-            wall_time_limit=500,
-            mem_in_gb=50,
-            array_params=f'1-{njobs}%16'
-        ))
+    makedirs(f"{cdir6}/logs", exist_ok=True)
+    with open(f"{cdir6}/step-6.slurm", mode="w", encoding="utf-8") as f:
+        f.write(
+            template6.render(
+                conda_environment="qp_pacbio_2025.9",
+                output=f"{out_dir}",
+                job_name=f"s6-{job_id}",
+                node_count=1,
+                nprocs=8,
+                wall_time_limit=500,
+                mem_in_gb=50,
+                array_params=f"1-{njobs}%16",
+            )
+        )
 
     template7 = jinja_env.get_template("7.checkm_batch.sbatch")
-    cdir7 = f'{out_dir}/step-7'
+    cdir7 = f"{out_dir}/step-7"
     makedirs(cdir7)
-    makedirs(f'{cdir7}/logs', exist_ok=True)
-    with open(f'{cdir7}/step-7.slurm', mode="w", encoding="utf-8") as f:
-        f.write(template7.render(
-            conda_environment='qp_pacbio_2025.9',
-            output=f'{out_dir}',
-            job_name=f's7-{job_id}',
-            node_count=1,
-            nprocs=8,
-            wall_time_limit=500,
-            mem_in_gb=50,
-            array_params=f'1-{njobs}%16'
-        ))
-    
+    makedirs(f"{cdir7}/logs", exist_ok=True)
+    with open(f"{cdir7}/step-7.slurm", mode="w", encoding="utf-8") as f:
+        f.write(
+            template7.render(
+                conda_environment="qp_pacbio_2025.9",
+                output=f"{out_dir}",
+                job_name=f"s7-{job_id}",
+                node_count=1,
+                nprocs=8,
+                wall_time_limit=500,
+                mem_in_gb=50,
+                array_params=f"1-{njobs}%16",
+            )
+        )
 
 
 def generate_sample_list(qclient, artifact_id, out_dir):
@@ -218,15 +232,15 @@ def generate_sample_list(qclient, artifact_id, out_dir):
     """
     files, prep = qclient.artifact_and_preparation_files(artifact_id)
 
-    lookup = prep.set_index('run_prefix')['sample_name'].to_dict()
+    lookup = prep.set_index("run_prefix")["sample_name"].to_dict()
     lines = []
     for _, (fwd, _) in files.items():
-        fwd = fwd['filepath']
+        fwd = fwd["filepath"]
         sname = search_by_filename(basename(fwd), lookup)
-        lines.append(f'{sname}\t{fwd}')
+        lines.append(f"{sname}\t{fwd}")
 
-    with open(f'{out_dir}/sample_list.txt', 'w') as f:
-        f.write('\n'.join(lines))
+    with open(f"{out_dir}/sample_list.txt", "w") as f:
+        f.write("\n".join(lines))
 
     return len(lines)
 
@@ -250,17 +264,17 @@ def pacbio_processing(qclient, job_id, parameters, out_dir):
     bool, list, str
         The results of the job
     """
-    results_fp = f'{out_dir}/results'
+    results_fp = f"{out_dir}/results"
     makedirs(results_fp)
 
     qclient.update_job_step(
-            job_id, "Step 1 of 3: Collecting info and generating submission")
-    artifact_id = parameters['artifact_id']
+        job_id, "Step 1 of 3: Collecting info and generating submission"
+    )
+    artifact_id = parameters["artifact_id"]
 
     njobs = generate_sample_list(qclient, artifact_id, out_dir)
 
-    qclient.update_job_step(
-            job_id, "Step 2 of 3: Creating submission templates")
+    qclient.update_job_step(job_id, "Step 2 of 3: Creating submission templates")
 
     generate_templates(out_dir, job_id, njobs)
 
@@ -272,34 +286,37 @@ def pacbio_processing(qclient, job_id, parameters, out_dir):
 
     # Re-render Step 2 with dependency on the corresponding task of Step 1
     # (Overwrites step-2/step-2.slurm to inject --dependency)
-    jinja_env = Environment(loader=KISSLoader('../data/templates'))
+    jinja_env = Environment(loader=KISSLoader("../data/templates"))
     template2 = jinja_env.get_template("2.get-circular-genomes.sbatch")
-    cdir2 = f'{out_dir}/step-2'
+    cdir2 = f"{out_dir}/step-2"
     makedirs(cdir2, exist_ok=True)
-    with open(f'{cdir2}/step-2.slurm', mode="w", encoding="utf-8") as f:
-        f.write(template2.render(
-            conda_environment='qp_pacbio_2025.9',
-            output=f'{out_dir}',
-            job_name=f's2-{job_id}',
-            node_count=1,
-            nprocs=1,
-            wall_time_limit=500,
-            mem_in_gb=16,
-            array_params=f'1:{njobs}%16',
-            dependency=(f'aftercorr:{jid1}' if jid1 else None)
-        ))
+    with open(f"{cdir2}/step-2.slurm", mode="w", encoding="utf-8") as f:
+        f.write(
+            template2.render(
+                conda_environment="qp_pacbio_2025.9",
+                output=f"{out_dir}",
+                job_name=f"s2-{job_id}",
+                node_count=1,
+                nprocs=1,
+                wall_time_limit=500,
+                mem_in_gb=16,
+                array_params=f"1:{njobs}%16",
+                dependency=(f"aftercorr:{jid1}" if jid1 else None),
+            )
+        )
 
     # Submit Step 2 (now chained to Step 1 with aftercorr)
     # jid2 = qclient.submit_job(f'{out_dir}/step-2/step-2.slurm')
 
-    qclient.update_job_step(
-            job_id, "Step 3 of 3: Running commands")
+    qclient.update_job_step(job_id, "Step 3 of 3: Running commands")
 
     # TODO 3. wait for all jobs to finish
 
-    qclient.update_job_step(
-            job_id, "Commands finished")
+    qclient.update_job_step(job_id, "Commands finished")
 
-    paths = [(f'{results_fp}/', 'directory')]
-    return (True, [ArtifactInfo('output', 'job-output-folder', paths)],
-            f'{job_id} completed.')
+    paths = [(f"{results_fp}/", "directory")]
+    return (
+        True,
+        [ArtifactInfo("output", "job-output-folder", paths)],
+        f"{job_id} completed.",
+    )
